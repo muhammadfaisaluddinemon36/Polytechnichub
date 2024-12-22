0415bd8731545ed1e770388aaac84a65f4aa3721
Muhammad Faisal Uddin Emon <developerfaisal1@gmail.com>
2024-12-22 16:00:37 +0600

    Update ServiceGenerator.java

--- app/src/main/java/com/dalazu/bteb/api/ServiceGenerator.java
+++ app/src/main/java/com/dalazu/bteb/api/ServiceGenerator.java
@@ -11,7 +11,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ServiceGenerator {
-    private static final String BASE_URL = "http://139.59.9.34/";
+
 
     private static final OkHttpClient okHttpClient = new OkHttpClient.Builder()
             .readTimeout(3600, TimeUnit.SECONDS)
